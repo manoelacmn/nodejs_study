10ab244e9ba09b5a80230eb5bb2177671eedffd8
silent <manoel.acmn@puccampinas.edu.br>
2022-11-05 21:38:34 -0300

    #3

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const app  = express()
 const rotaProdutos = require('./routes/produtos')
 
 const rotaPedidos = require('./routes/pedidos')
+const morgan = require('morgan')
 
+
+app.use(morgan('dev'))
 app.use('/produtos',rotaProdutos);
-
 app.use('/pedidos',rotaPedidos);
 
 
@@ -16,6 +18,22 @@ app.use('/teste',(req,res,end) => {
   res.status(200).send({
     mensagem: 'ok,Deu certo'
   });
+})
+
+
+app.use((req,res,next) => {
+  const erro = new Error('not found')
+  erro.status = 404;
+  next(erro)
+})
+
+app.use((error,req,res,next) => {
+    res.status(error.status || 500);
+    return res.send({
+      error: {
+        mensagem: error.message
+      }
+    })
 })
 
 module.exports = app;
